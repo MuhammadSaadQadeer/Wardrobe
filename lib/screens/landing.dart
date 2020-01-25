a7fc6bf004bab697a09077b274557bc25eeeb3be
MuhammadSaadQadeer <saadqadeer59@gmail.com>
2020-01-25 21:50:48 +0500

    refactor: update icons

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -15,7 +15,7 @@ class Landing extends StatelessWidget {
           SizedBox(
             width: 300, // specific value
             child: CustomIconButton(
-                imagePath: 'assets/facebooklogo.jpg',
+                imagePath: 'assets/facebooklogo.png',
                 btnText: "Sign In With Facebook",
                 bgColor: Colors.white),
           ),
@@ -25,7 +25,7 @@ class Landing extends StatelessWidget {
           SizedBox(
             width: 300, // specific value
             child: CustomIconButton(
-                imagePath: 'assets/googlelogo.jpg',
+                imagePath: 'assets/googlelogo.png',
                 btnText: "Sign In With Google",
                 bgColor: Colors.white),
           ),
